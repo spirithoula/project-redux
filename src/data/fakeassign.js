c6473a08fdde9235a3a3ae7b56ae8e21b2620d96
Alex Harrington <spirithoula2@gmail.com>
2021-09-16 17:34:40 -0500

    adding fake assignments

---
+++ src/data/fakeassign.js
@@ -0,0 +1,33 @@
+
+
+const grades = [
+    {
+        id: 1,
+        projTitle: 'Planner',
+        projReqs: ['Responsive', 'Good ReadME'],
+        dueDate: "2021-04-17",
+        varCats: ['Category 1', 'Category 2'],
+        varCatsVals: ['Value 1', 'Value 2'],
+        groupSize: 5,
+        groupScore: ['Score'],
+        memberScore: ['Score 1 mem 1'],
+        links: ['deployed link', 'github link'],
+        comments: ['Needs work', 'Needs a proper readme'],
+        gradeCalc: 70,
+    },
+    {
+        id: 2,
+        projTitle: 'Notes',
+        projReqs: ['Responsive', 'Polished UI', 'Deployed Successfully'],
+        dueDate: "2021-06-01",
+        varCats: ['Category 1', 'Category 2'],
+        varCatsVals: ['Value 1', 'Value 2'],
+        groupSize: 3,
+        groupScore: ['Score'],
+        memberScore: ['Score 1 mem 3'],
+        links: ['deployed link', 'github link'],
+        comments: ['Good Work', 'Styling looked great'],
+        gradeCalc: 80,
+    },
+]
+export default grades
