0ef640d657108151b7c36e16edb66e1b71475a16
Alex Harrington <spirithoula2@gmail.com>
2021-09-16 17:12:37 -0500

    adding to calendar component

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,4 +1,14 @@
 import React from "react";
+import moment from "moment";
+
+weekdayshort = moment.weekdaysShort();
+let weekdayshortname = this.weekdayshort.map(day => {
+    return (
+      <th key={day} className="week-day">
+       {day}
+      </th>
+    );
+ });
 
 export default class Calendar extends React.Component {
   render() {
